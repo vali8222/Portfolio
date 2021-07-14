b1361a097c2d9da689a4fc72455336e18e909ab2
vali8222 <69923430+vali8222@users.noreply.github.com>
2021-07-14 16:05:18 +0300

    Add files via upload
    first commit

---
+++ Portofolio/script.js
@@ -0,0 +1,12 @@
+//  NAVIGATION BURGER MENU
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+
+
